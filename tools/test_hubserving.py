2d39c5a124e4fc838ce8545a6d5c151bba372ebc
azhar0100 <azhartahir0100@gmail.com>
2022-04-23 09:17:24 +0500

    Reverted module name changes

--- tools/test_hubserving.py
+++ tools/test_hubserving.py
@@ -25,7 +25,7 @@ import numpy as np
 import time
 from PIL import Image
 from ppocr.utils.utility import get_image_file_list
-from paddleocr.tools.infer.utility import draw_ocr, draw_boxes
+from tools.infer.utility import draw_ocr, draw_boxes
 
 import requests
 import json
